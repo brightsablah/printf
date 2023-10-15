199b394f276ff272985bb18e0c1953c524163e8a
brikhotso <brenda.rikhotso@gmail.com>
2023-10-15 01:15:02 -0700

    updated header file

--- main.h
+++ main.h
@@ -1,14 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdarg.h> /* for variadic function */
+#include <stdlib.h> /* for malloc and free */
+#include <unistd.h> /* used by write */
 
+int _print_number(int num);
 int b_putchar(char b);
+int b_putstr(char *str);
+int print_b_char(va_list args);
+int print_b_string(va_list args);
+int print_b_integer(va_list args);
+int print_b_float(va_list args);
 int _printf(const char *format, ...);
-
 
 
 #endif
