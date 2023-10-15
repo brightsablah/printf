8d3738cbffa7584258b5e2d7c501920c5167f3bd
brikhotso <brenda.rikhotso@gmail.com>
2023-10-14 18:49:41 -0700

    task 0 initial submission

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int b_putchar(char b);
+int _printf(const char *format, ...);
+
+
+
+#endif
