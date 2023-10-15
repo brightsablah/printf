7e231d3fa51f18dc3d355cfa93a0903eccbb4d73
Bright <bright.sablah@outlook.com>
2023-10-15 03:29:02 +0000

    new trial

--- _printf.c
+++ _printf.c
@@ -1,57 +1,57 @@
 #include "main.h"
+#include <string.h>
+typedef int (*format_handler)(va_list);
 
-int _printf(const char *format, ...)
-{
-	char *buffer;
-	const char *str;
-	int buffer_index = 0, buffer_size = 0;
-	va_list arg_list;
-
-	va_start(arg_list, format);
-
-	if (format == NULL)
-		return (-1);
-	while (format[buffer_size] != '\0')
-		buffer_size++;
-
-	buffer = malloc(buffer_size + 1);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					buffer[buffer_index++] = (char)va_arg(arg_list, int);
-					break;
-				case 's':
-				{
-					str = va_arg(arg_list, const char *);
-					while (*str)
-					{
-						buffer[buffer_index++] = *str++;
-					}
-					break;
-				}
-				case '%':
-					buffer[buffer_index++] = '%';
-					break;
-				default:
-					break;
-				}
-			}
-			else
-			{
-				buffer[buffer_index++] = *format;
-			}
-			format++;
-	}
-		buffer[buffer_index++] = '\0';
-		va_end(arg_list);
-
-		write(1, buffer, buffer_index);
-		free(buffer);
-		return (buffer_index);
+int handle_c(va_list arg_list) {
+    return write(1, (char[]){va_arg(arg_list, int)}, 1);
 }
+
+int handle_s(va_list arg_list) {
+    const char *str = va_arg(arg_list, const char *);
+    if (str == NULL) str = "(null)";
+    return write(1, str, (int)strlen(str));
+}
+
+int handle_percent(va_list arg_list) {
+    (void)arg_list;
+    return write(1, "%", 1);
+}
+
+static const struct {
+    char specifier;
+    format_handler handler;
+} format_handlers[] = {
+    {'c', handle_c},
+    {'s', handle_s},
+    {'%', handle_percent},
+};
+
+int _printf(const char *format, ...) {
+    if (format == NULL) return -1;
+
+    va_list arg_list;
+    va_start(arg_list, format);
+
+    int count = 0;
+    char format_char;
+    
+    while ((format_char = *format++)) {
+        if (format_char != '%') {
+            count += write(1, &format_char, 1);
+        } else {
+            format_handler handler = NULL;
+            for (size_t i = 0; i < sizeof(format_handlers) / sizeof(format_handlers[0]); i++) {
+                if (format_handlers[i].specifier == *format) {
+                    handler = format_handlers[i].handler;
+                    break;
+                }
+            }
+            count += handler ? handler(arg_list) : write(1, "%", 1);
+            format++;
+        }
+    }
+
+    va_end(arg_list);
+    return count;
+}
+
