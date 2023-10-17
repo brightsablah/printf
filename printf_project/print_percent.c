59b2fe43b439174c62eceae86e646440bfc099c6
Bright <bright.sablah@outlook.com>
2023-10-17 04:22:51 +0000

    print_percent

---
+++ printf_project/print_percent.c
@@ -0,0 +1,14 @@
+/**
+* print_percent - Prints a percent sign
+* @arg: A va_list (unused)
+*
+* Description:
+* This function prints a percent sign ("%") to the standard output.
+*/
+void print_percent(va_list arg)
+{
+	(void)arg; /* Unused parameter */
+
+	_putchar('%');
+}
+
