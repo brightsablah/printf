ce610a27fefb4c57d5f2d5ad511f3b9c67bc2731
Bright <bright.sablah@outlook.com>
2023-10-17 04:50:53 +0000

    back up of individual functions with documentation

---
+++ backup/print_rot13.c
@@ -0,0 +1,31 @@
+/**
+* print_rot13 - Prints a string encoded in ROT13
+* @arg: A va_list containing the string to print
+*
+* Description:
+* This function prints a string encoded in ROT13 to the standard output.
+*/
+void print_rot13(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+	char *rot13_str;
+	int i;
+
+/* special case for NULL string */
+	if (str == NULL)
+		return;
+
+/* creating a copy of string so i don't edit the original string */
+	rot13_str = _strdup(str);
+	if (rot13_str == NULL) /* checking if string copying was succesful */
+	return;
+
+/* converting copied string to a ROT13 string */
+	rot13(rot13_str);
+
+/* printing ROT13 string */
+	for (i = 0; rot13_str[i] != '\0'; i++)
+	{
+		_putchar(rot13_str[i]);
+	}
+}
