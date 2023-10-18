73c1af0d68e52de6a8f63fdffb72ee956d6a881c
brikhotso <brenda.rikhotso@gmail.com>
2023-10-17 19:31:32 -0700

    added buffer description

--- print_int.c
+++ print_int.c
@@ -2,6 +2,7 @@
 /**
 * print_int - Prints an integer
 * @arg: A va_list containing the integer to print
+* @buffer: pointer a buffer that stores print output
 *
 * Description:
 * This function prints an integer to the standard output.
