be0f997923539df3a885ce541c04f2dfcd317e04
brikhotso <brenda.rikhotso@gmail.com>
2023-10-17 19:31:07 -0700

    added buffer description

--- print_hex_upper.c
+++ print_hex_upper.c
@@ -2,6 +2,7 @@
 /**
 * print_hex_upper - Prints an integer in hexadecimal (uppercase)
 * @arg: A va_list containing the integer to print
+* @buffer: pointer a buffer that stores print output
 *
 * Description:
 * This function prints an integer in hexadecimal (uppercase)
