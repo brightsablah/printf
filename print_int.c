bc0967330641ca22a19c97633ff254ee796c27c3
Bright <bright.sablah@outlook.com>
2023-10-22 08:51:43 +0000

    precision and width setup

--- print_int.c
+++ print_int.c
@@ -7,9 +7,39 @@
 * Description:
 * This function prints an integer to the standard output.
 */
-void print_int(va_list arg, Buffer *buffer)
+void print_int(va_list arg, Buffer *buffer, format_options *options)
 {
-	int num = va_arg(arg, int);
+	int num, len, spaces, zeroes;
+
+	num = va_arg(arg, int);
+	len = number_length(num);
+	spaces = options->width - len;
+
+	while (spaces > 0)
+	{
+		buffer_append_char(buffer, ' ');
+		spaces--;
+	}
+
+	if (options->precision > len)
+	{
+		zeroes = options->precision - len;
+		if (num < 0)
+		{
+			buffer_append_char(buffer, '-');
+			num = -num;
+			zeroes++;
+		}
+		while (zeroes > 0)
+		{
+			buffer_append_char(buffer, '0');
+			zeroes--;
+		}
+	}
+	else if (options->precision == 0)
+	{
+		return;
+	}
 
 	_print_number(num, buffer);
 }
