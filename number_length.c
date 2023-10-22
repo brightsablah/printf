3836227acbaa24f139634e004cdf613b0aaa1192
Bright <bright.sablah@outlook.com>
2023-10-22 08:55:31 +0000

    added number length check function

---
+++ number_length.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int number_length(int num)
+{
+	int length = 0;
+
+	if (number == 0)
+	{
+		return (1);
+	}
+
+	if (num < 0)
+	{
+		length++;
+		num = -num;
+	}
+
+	while (num > 0)
+	{
+		length++;
+		num /= 10;
+	}
+
+	return (length);
+}
